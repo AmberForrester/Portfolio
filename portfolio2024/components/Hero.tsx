1c0b79f2416aff939b77e92304183088355633f7
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-08-25 04:51:07 -0400

    Personalized message on main page.

--- portfolio2024/components/Hero.tsx
+++ portfolio2024/components/Hero.tsx
@@ -30,14 +30,14 @@ const Hero = () => {
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-wideset text-xs text-center text-blue-100 max-w-80'>
-                Dynamic Web Magic with Next.js
+                Designing the Future, One Experience at a Time
                 </h2>
 
                 {/**
                 *  Link: https://ui.aceternity.com/components/text-generate-effect*/}
                 <TextGenerateEffect
                 className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                words='Transforming Concepts into Seamless User Experiences'/>
+                words='Turning Ordinary Ideas into Extraordinary Experiences'/>
 
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                     Hello I&apos;m Amber, a retired Nurse turned Full Stack Developer based in Canada.
--- portfolio2024/components/ui/TextGenerateEffect.tsx
+++ portfolio2024/components/ui/TextGenerateEffect.tsx
@@ -39,7 +39,7 @@ export const TextGenerateEffect = ({
             <motion.span
               key={word + idx}
               className={` ${
-                idx > 3 ? "text-purple" : "dark:text-white text-black"
+                idx > 4 ? "text-purple" : "dark:text-white text-black"
               } opacity-0`}
             >
               {word}{" "}
