d145ce58e6246f7b3e0c5b934e3b9414f93021b5
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-08-25 04:41:38 -0400

    Portfolio icon added

--- portfolio2024/app/layout.tsx
+++ portfolio2024/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/portfolio_icon.ico"/>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
