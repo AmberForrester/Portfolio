a1969e0a1c186909111795d53504eac7035ed784
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-20 15:18:36 -0400

    Recent Projects

--- portfolio2024/app/page.tsx
+++ portfolio2024/app/page.tsx
@@ -9,9 +9,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: "Home", link: "/"}
-        ]}/>
+        <FloatingNav navItems={/>
         <Hero />
         <Grid />
         <RecentProjects />
--- portfolio2024/components/RecentProjects.tsx
+++ portfolio2024/components/RecentProjects.tsx
@@ -12,11 +12,12 @@ const RecentProjects = () => {
             <span className="text-purple">Recent Projects</span>
         </h1>
 
-        <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
             {projects.map(({ id, title, des, img, iconLists, link }) => (
-                <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+                <div key={id} className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w-[80vw]">
+
                     <PinContainer title={link} href={link}>
-                        <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                        <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                             <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                                 <img src="/bg.png" alt="bg-img" />
                             </div>
