ec9c32afcd17410d1b6bb3c8263adbb64fd0e1dc
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-21 11:18:14 -0400

    Approach

--- portfolio2024/components/Approach.tsx
+++ portfolio2024/components/Approach.tsx
@@ -7,7 +7,7 @@ const Approach = () => {
   return (
     <section className="w-full py-20">
       <h1 className="heading">
-        My unique<span className="text-purple">approach</span>
+        My unique <span className="text-purple">approach</span>
       </h1>
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         <Card
--- portfolio2024/next.config.mjs
+++ portfolio2024/next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    swcMinify: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    }
-    // Add any additional configurations here if needed
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // This will skip ESLint during builds
+  }
+  // Add any additional configurations here if needed
+};
+
+export default nextConfig;
