8760bf2a9f2a77ef6aa62d0e4bf6baf10382b3c5
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-21 08:40:40 -0400

    Floating Navbar

--- portfolio2024/components/Hero.tsx
+++ portfolio2024/components/Hero.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36" id="about">
+    <div className="pb-20 pt-36">
 
         {/**
        *  UI: Spotlights
