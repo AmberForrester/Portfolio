5c8151962e5115ce207342ac3acd894605dde221
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-20 22:00:46 -0400

    Work Experience

--- portfolio2024/app/page.tsx
+++ portfolio2024/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 
 
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
