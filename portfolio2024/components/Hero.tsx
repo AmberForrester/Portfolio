8a1facbc20efda97363f8d2ca24b6bd0d0701d05
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-20 21:42:39 -0400

    Hero Section

--- portfolio2024/components/Hero.tsx
+++ portfolio2024/components/Hero.tsx
@@ -12,20 +12,29 @@ const Hero = () => {
        *  Link: https://ui.aceternity.com/components/spotlight
        */}
         <div>
-            <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
-
-            <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple"/>
-
-            <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue"/>
+            <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+            />
+            <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+            />
+            <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
         </div>
 
         {/**
        *  UI: grid
        * Link: https://ui.aceternity.com/components/grid-and-dot-backgrounds*/}
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.00] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center">
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-             bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-        </div>
+        <div
+            className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+            absolute top-0 left-0 flex items-center justify-center"
+        >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
 
         <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
@@ -37,8 +46,9 @@ const Hero = () => {
                 {/**
                 *  Link: https://ui.aceternity.com/components/text-generate-effect*/}
                 <TextGenerateEffect
+                words="Turning Ordinary Ideas into Extraordinary User Experiences"
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Turning Ordinary Ideas into Extraordinary User Experiences"/>
+                />
                 <br/>
 
                 <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
@@ -57,7 +67,7 @@ const Hero = () => {
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
