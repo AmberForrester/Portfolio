757e5695da4ee146e9de059cbf1464cc564ab504
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-22 13:17:18 -0400

    Clients

--- portfolio2024/components/Clients.tsx
+++ portfolio2024/components/Clients.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 import { companies, testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
+import Image from "next/image";
 
 const Clients = () => {
   return (
@@ -27,14 +28,17 @@ const Clients = () => {
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
+                <Image
                   src={company.img}
                   alt={company.name}
                   className="md:w-10 w-5"
+                  height={180}
+                  width={180}
                 />
-                <img
+                <Image
                   src={company.nameImg}
                   alt={company.name}
+                  height={200}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 />
