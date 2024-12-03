d57f1241d0a877f8f674c9e4d25c5b758c9680fa
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-03 01:14:58 -0500

    Featured Projects updated

--- portfolio2024/components/RecentProjects.tsx
+++ portfolio2024/components/RecentProjects.tsx
@@ -80,7 +80,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="font-bold flex lg:text-xl md:text-xs text-sm text-purple">
-                    More Details
+                    Explore
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
--- portfolio2024/data/index.ts
+++ portfolio2024/data/index.ts
@@ -72,51 +72,51 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "Quick Gist | AI Summarizer",
-    des: "Article summarizer application using OpenAI's GPT model",
-    img: "/p5.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/https://quickgist.amberforrester.io",
+    title: "Zeniverse: Portal to the Metaverse",
+    des: "Captures the essence of a harmonious and expansive digital experience",
+    img: "/zeniverse.png",
+    iconLists: ["/re.svg", "/vite.svg", "/tail.svg", "/gsap.svg", "/js.svg", "/clippy.png"],
+    link: "https://zeniverse.vercel.app",
   },
   {
     id: 2,
-    title: "CarePulse | Patient Management System",
-    des: "Healthcare platform that streamlines secure patient registration, appointment scheduling with SMS notifications.",
-    img: "/p5.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/https://carepulse.amberforrester.io",
+    title: "Evently: Event Management Platform",
+    des: "Enhance your ablity to create, organize, and manage events",
+    img: "/evently.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/nodejs.svg", "/mongodb.svg"],
+    link: "https://evently-pink-sigma.vercel.app",
   },
   {
     id: 3,
-    title: "DevSearch",
+    title: "StoreIt: Your Personal Cloud",
     des: "Online platform where Developers from around the world can create a profile, engage with other developers while sharing their projects.",
-    img: "/p5.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/https://devsearch.amberforrester.io",
+    img: "/storeit.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/figma.svg", "/appwrite.png"],
+    link: "https://www.storeit.amberforrester.io",
   },
   {
     id: 4,
-    title: "StackSociety",
+    title: "LiveDocs: Real-Time Collaboration",
     des: "A community-focused hub for full stack engineering insights.",
-    img: "/p5.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/https://stacksociety.amberforrester.io",
+    img: "/livedocs.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/nodejs.svg", "/clerk.png", "/liveblocks.png"],
+    link: "https://livedocs-livid.vercel.app",
   },
   {
     id: 5,
-    title: "JobZila",
-    des: "You dream job awaits on this job search application.",
-    img: "/p5.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/https://jobzila.amberforrester.io",
+    title: "Quick Gist: AI Summarizer",
+    des: "Article summarizer application using OpenAI's GPT model",
+    img: "/quickgist.png",
+    iconLists: ["/openai.png", "/tail.svg", "/js.svg", "/vite.svg", "/redux.svg", "/rapidapi.png"],
+    link: "https://quickgist.amberforrester.io",
   },
   {
     id: 6,
-    title: "UberClone",
-    des: "Everyone's favourite ride share application.",
-    img: "/p5.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "//https://uberclone.amberforrester.io",
+    title: "Modern Next.js Portfolio",
+    des: "Beautifully crafted portfolio, personal accomplishment from dedication and commitment",
+    img: "/portfolio.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/aceternityui.png"],
+    link: "https://www.amberforrester.io",
   },
 ];
 
@@ -133,7 +133,7 @@ export const innovation_pipeline = [
       "StackSociety is a dedicated platform where full-stack software engineers come together to share invaluable insights, experiences, and resources. It’s a hub for developers to showcase their journeys, offer tips and tricks, and recommend books that have significantly shaped their growth. Built with Next.js, StackSociety is designed to foster collaboration, enhance the open-source community, and amplify the value of shared knowledge. It’s more than a blog—it’s a movement to support and uplift developers across the globe.",
     name: "Stack Society",
     title: "Empowering Developers Together",
-    src: "/StackSociety.png",
+    src: "/stacksociety.png",
   },
   {
     desc:
@@ -144,43 +144,10 @@ export const innovation_pipeline = [
   }
 ];
 
-export const companies = [
-  {
-    id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
-  },
-];
-
 export const workExperience = [
   {
     id: 1,
-    title: "Innovation-Driven Full Stack Software Engineer",
+    title: "Innovation Driven Full Stack Software Engineer",
     desc: "Pioneered full stack applications, balancing both front-end and back-end responsibilities.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
@@ -188,14 +155,14 @@ export const workExperience = [
   {
     id: 2,
     title: "RESTful API Lead",
-    desc: "Optimize system integration and streamlining secure data flow between applications.",
+    desc: "Optimized system integrations and streamlined secure data flow between applications.",
     className: "md:col-span-2",
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
     title: "Scalable Solutions Architect",
-    desc: "Delivered high imactful solutions that align with business needs, with long-term growth potential. ",
+    desc: "Tailored to specific business needs, ensuring alignment with long-term growth strategies and objectives.",
     className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
