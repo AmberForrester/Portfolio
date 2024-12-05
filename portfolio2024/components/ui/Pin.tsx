dc41fff2636f1bb0e4717d0347a1796403202978
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 14:21:17 -0500

    http link UI updated to fit

--- portfolio2024/components/ui/Pin.tsx
+++ portfolio2024/components/ui/Pin.tsx
@@ -75,7 +75,7 @@ export const PinPerspective = ({
             href={href || "#"}
             target={href ? "_blank" : "_self"}
             rel={href ? "noopener noreferrer" : undefined}
-            className="relative flex items-center justify-center z-10 rounded-full bg-zinc-950 py-1 px-4 ring-1 ring-white/10 text-center overflow-hidden whitespace-nowrap text-ellipsis"
+            className="relative flex items-center justify-center z-10 rounded-full bg-zinc-950 py-1 px-6 ring-1 ring-white/10 text-center overflow-hidden whitespace-nowrap text-ellipsis"
             style={{ maxWidth: "calc(100% - 2rem)", width: "fit-content" }}
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
