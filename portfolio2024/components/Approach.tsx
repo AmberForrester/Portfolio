5083b6ad0e2a2aee31e2fef321a958649f4556de
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 14:29:20 -0500

    Approach UI updated

--- portfolio2024/components/Approach.tsx
+++ portfolio2024/components/Approach.tsx
@@ -125,7 +125,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
          bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
         />
         <span
-          className="inline-flex h-full w-full cursor-pointer items-center 
+          className="inline-flex h-full w-full items-center 
         justify-center rounded-full bg-slate-950 px-5 py-2 text-purple backdrop-blur-3xl font-bold text-2xl"
         >
           {order}
