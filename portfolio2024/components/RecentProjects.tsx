e3d039346966b42b18b265ccfd8e469c0b55635e
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 12:36:33 -0500

    Recent Project Layout updated

--- portfolio2024/components/RecentProjects.tsx
+++ portfolio2024/components/RecentProjects.tsx
@@ -12,12 +12,12 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="block sm:inline text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div key={id}
-            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[10vh] lg:h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <Image 
                   src="/bg.png" 
