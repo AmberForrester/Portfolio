d4e420175e44b172f961f9972dfba63e4f5eba8f
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 11:29:05 -0500

    favicon updated

--- portfolio2024/app/layout.tsx
+++ portfolio2024/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/portfolio_icon.ico"/>
+        <link rel="icon" href="/favicon.ico"/>
       </head>
       <body className={inter.className}>
         <ThemeProvider
