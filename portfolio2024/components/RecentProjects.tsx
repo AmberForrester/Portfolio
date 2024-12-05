c63f2ca704396faf1dc925e09a565f1650fa08ed
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 12:21:16 -0500

    Recent Project UI/UX update

--- portfolio2024/components/RecentProjects.tsx
+++ portfolio2024/components/RecentProjects.tsx
@@ -12,13 +12,12 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="block sm:inline text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-          >
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[10vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <Image 
                   src="/bg.png" 
@@ -54,10 +53,10 @@ const RecentProjects = () => {
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
                     <div
-                      key={index}
+                      key={icon}
                       className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
+                        transform: `translateX(-${5 * index * 2}px)`,
                       }}
                     >
                       <Image 
@@ -72,7 +71,7 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="font-bold flex lg:text-xl md:text-xs text-sm text-purple">
+                  <p className="flex font-bold lg:text-xl md:text-xs text-sm text-purple">
                     Explore
                   </p>
                   <FaLocationArrow className="ms-2" color="#CBACF9" />
