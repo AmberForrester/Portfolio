b0d04fc0123d3ab9370bcc99e135a168df3aa25d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-05 14:14:02 -0500

    Project Live Links updated

--- portfolio2024/components/ui/Pin.tsx
+++ portfolio2024/components/ui/Pin.tsx
@@ -69,13 +69,14 @@ export const PinPerspective = ({
 }) => {
   return (
     <motion.div className="w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
-      <div className=" w-full h-full -mt-7 flex-none  inset-0">
-        <div className="absolute top-0 inset-x-0  flex justify-center">
+      <div className="w-full h-full -mt-7 flex-none inset-0">
+        <div className="relative flex justify-center items-center">
           <a
             href={href || "#"}
             target={href ? "_blank" : "_self"}
             rel={href ? "noopener noreferrer" : undefined}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+            className="relative flex items-center justify-center z-10 rounded-full bg-zinc-950 py-1 px-4 ring-1 ring-white/10 text-center overflow-hidden whitespace-nowrap text-ellipsis"
+            style={{ maxWidth: "calc(100% - 2rem)", width: "fit-content" }}
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
