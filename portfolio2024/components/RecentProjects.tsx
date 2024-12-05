755778854f93ef64c90aae294685c8e728a68277
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-04 21:15:10 -0500

    Mobile Screen adjustment

--- portfolio2024/components/RecentProjects.tsx
+++ portfolio2024/components/RecentProjects.tsx
@@ -10,23 +10,16 @@ const RecentProjects = () => {
     <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        <span className="block sm:inline text-purple">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-        {projects.map((item) => (
-          <div
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div key={id}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
           >
-            <PinContainer
-              title={item.link}
-              href="https://github.com/AmberForrester"
-            >
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[10vh] lg:h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <Image 
                   src="/bg.png" 
                   alt="bgimg" 
@@ -35,16 +28,16 @@ const RecentProjects = () => {
                   />
                 </div>
                 <Image
-                  src={item.img}
-                  alt="cover"
+                  src={img}
+                  alt={title}
                   className="z-10 absolute inset-0 w-full h-full object-cover"
-                  height={500}
-                  width={500}
+                  height={100}
+                  width={100}
                 />
               </div>
 
               <h1 className="font-bold lg:text-1xl md:text-xl text-base line-clamp-1">
-                {item.title}
+                {title}
               </h1>
 
               <p
@@ -54,22 +47,22 @@ const RecentProjects = () => {
                   margin: "1vh 0",
                 }}
               >
-                {item.des}
+                {des}
               </p>
 
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
+                  {iconLists.map((icon, index) => (
                     <div
                       key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
                       <Image 
                       src={icon} 
-                      alt="icon5" 
+                      alt={icon}
                       className="p-2" 
                       height={100}
                       width={100}
