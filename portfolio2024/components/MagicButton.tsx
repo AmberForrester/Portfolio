59c8caa250cdf93c5916322e0402c1b604fae72a
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-04 23:01:16 -0500

    Magic Button UI updated

--- portfolio2024/components/MagicButton.tsx
+++ portfolio2024/components/MagicButton.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-
 const MagicButton = ({
   title,
   icon,
@@ -17,10 +15,10 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1.5px] focus:outline-none"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] -z-10" />
 
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
