1ad2d92b4be9053b3559455afaed60d651899039
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-09 11:56:31 -0500

    Mobile UI update

--- portfolio2024/app/projects/[slug]/page.tsx
+++ portfolio2024/app/projects/[slug]/page.tsx
@@ -33,13 +33,13 @@ export default function ProjectDetails({ params }: { params: { slug: string } })
   };
 
   return (
-    <div className="p-8 project-container-mobile">
+    <div className="p-8">
       <BackButton />
 
       {/* HeroParallax with the customized project data */}
       <HeroParallax selectedProject={selectedProject} />
 
-      <section className="mt-[-120vh] px-10 pb-[30vh] project-features-mobile">
+      <section className="mt-[-120vh] px-10 pb-[30vh]">
         <div className="max-w-7xl mx-auto">
           <h1 className="!text-left heading">
             Project <span className="block sm:inline text-purple">Features</span>
